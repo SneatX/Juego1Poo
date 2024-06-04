c07aa3417bb9a87d6533aa9e327d45d0ede44c10
x <x>
2024-06-04 16:17:18 -0500

    feat: :sparkles: Hero solo puede tener una instancia

--- js/modules/characters/hero.js
+++ js/modules/characters/hero.js
@@ -3,6 +3,11 @@ import { Creature } from "../creature.js"
 export class Hero extends Creature{
     constructor(name, maxLife, damage){
         super(name, maxLife, damage)
+        
+        if(Hero.instance){
+            return Hero.instance
+        }
+        Hero.instance = this
 
     }
 }
